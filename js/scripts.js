f472142ae8d94e596b64d579b707cea2e7f5c999
Student <example@example.com>
2018-08-17 09:29:42 -0700

    complete spec 3, program returns number range in order.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,23 @@ $(document).ready(function() {
 
   var userNumber = parseInt($("input#inputNumber").val());
 
+  var numbers = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  var beepBoop = [];
+
+loop1:
+  for(var i = 1; i < userNumber + 2; i++){
+    var examineNumber = numbers[i];
+    beepBoop.push(examineNumber);
+  } //for loop 1
+
+  console.log(examineNumber);
+  console.log(beepBoop);
 
 
 
-    $(".answer").text(userNumber)
 
-  });
-});
+    beepBoop = beepBoop.join(", ");
+    $(".answer").text(beepBoop);
+
+  }); // (#formOne).submit close
+}); // (document).ready close
