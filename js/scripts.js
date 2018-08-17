7d5d6a35fac758b9fd9febf082069a6ec62a66ce
Student <example@example.com>
2018-08-17 16:52:06 -0700

    add reverse function and button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
+
   $("#formOne").submit(function() {
-    $(".magicText").hide();
-    $(".well").show();
-  event.preventDefault();
-
-  var userNumber = parseInt($("input#inputNumber").val());
-  var numbers = ["0",...Array(1000).keys()];
-  var beepBoop = [];
-
-  for(var i = 1; i < userNumber + 2; i++){
-    var examineNumber = numbers[i];
-    if (examineNumber % 3 == 0) {
+      $(".magicText").hide();
+      $(".well").show();
+    event.preventDefault();
+    var userNumber = parseInt($("input#inputNumber").val());
+    var numbers = ["0",...Array(1000).keys()];
+    var beepBoop = [];
+    for(var i = 1; i < userNumber + 2; i++){
+      var examineNumber = numbers[i];
+      if (examineNumber % 3 == 0) {
           beepBoop.push("I'm sorry, Dave. I'm afraid I can't do that.");
       } else if (/1+/.test(examineNumber)) {
           beepBoop.push("Boop");
@@ -19,10 +18,34 @@ $(document).ready(function() {
       } else {
           beepBoop.push(examineNumber);
       }
-    }
-
+    };
     beepBoop = beepBoop.join(", ");
     $("#nameForMessage").text($("input#inputName").val());
     $("#answer").text(beepBoop + ".");
+  }); // (#formOne).submit close
+
+  $("#reverseButton").click(function() {
+      $(".magicText").hide();
+      $(".well").show();
+    event.preventDefault();
+    var userNumber = parseInt($("input#inputNumber").val());
+    var numbers = ["0",...Array(1000).keys()];
+    var beepBoop = [];
+    for(var i = 1; i < userNumber + 2; i++){
+      var examineNumber = numbers[i];
+      if (examineNumber % 3 == 0) {
+            beepBoop.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      } else if (/1+/.test(examineNumber)) {
+          beepBoop.push("Boop");
+      } else if (/0+/.test(examineNumber)) {
+          beepBoop.push("Beep");
+      } else {
+          beepBoop.push(examineNumber);
+      }
+    };
+      var beepBoopReversed = beepBoop.reverse();
+      beepBoopReversed = beepBoopReversed.join(", ");
+    $("#nameForMessage").text($("input#inputName").val());
+    $("#answer").text(beepBoopReversed + ".");
   }); // (#formOne).submit close
 }); // (document).ready close
