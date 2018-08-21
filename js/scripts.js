223d1274035c0b382bb6f52951fbe6f48f7dc879
Hingyi Khong and Talia Haller <hingyi+talia@>
2018-08-21 16:23:29 -0700

    add inefficient code, including a loop to extract value from radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,65 @@ function Movie(name,date) {
   this.movieDate = date;
 }
 
-var crazyRichAsians = new Movie("Crazy Rich Asians", "2018");
-var theIndcredibles = new Movie("The Incredibles","2001");
-var spaceOdyssey = new Movie("2001: Space Odyssey", "1987");
+var movieArray = [];
 
+var crazyRichAsians = new Movie("Crazy Rich Asians", 2018);
+var theIncredibles = new Movie("The Incredibles",2001);
+var spaceOdyssey = new Movie("2001: Space Odyssey", 1987);
 
+movieArray.push(crazyRichAsians);
+movieArray.push(theIncredibles);
+movieArray.push(spaceOdyssey);
 // User Interface Logic
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
 
-      // var inputtedMovie = $
 
-      var inputtedTime = $("input#timeInput").val();
-      var inputtedAge = $("input#AgeInput").val();
+      var movies = document.getElementsByName("exampleRadios");
+      var moviePrice = [];
+
+      for (var i = 0, length = movies.length; i < length; i++)
+        { if (movies[i].checked)
+          {  var customerMovie = movies[i].value;
+          break;
+          }
+        }
+        var n = new Date().getFullYear();
+        if (movieArray[customerMovie].movieDate <  n ) {
+          var priceOld = 1;
+          moviePrice.push(priceOld);
+        } else {
+          var priceNew = 3;
+          moviePrice.push(priceNew);
+        };
+
+        // // console.log(moviePrice);
+        //
+        // var inputtedTime = $("option:selected").val();
+        // console.log(inputtedTime);
+        //
+        // var inputtedMovie = $('input[name=exampleRadios]:checked').val();
+        // console.log(inputtedMovie);
+
+
+
+
+
+
+
+        // customerMovieDate =
+
+        // if (customerMovieDate < this.year) {
+        //
+        // }
+
+
+
+
+      // var inputtedTime = $("input#timeInput").val();
+      // var inputtedAge = $("input#AgeInput").val();
+
 
     // if (inputtedMovie == "old")
 
