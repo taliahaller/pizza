c8a5a9948088d775587f28e1ffaeab7a3f58e623
Nikki Wong and Talia Haller <nikki@>
2018-08-22 10:40:37 -0700

    add roll and currentscore functionality and constructor objects

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,22 @@ function rollValue(){
   return Math.floor((Math.random() * 6) + 1);
 }
 
+function Turn(player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2;
+}
+
+function Players(rollValue, currentScoreValue, totalScore) {
+  this.rollValue = rollValue;
+  this.currentScoreValue = currentScoreValue;
+  this.totalScore = totalScore;
+}
+
+
+// var playerOne = new Players(output1, player1CurrentScore, player1TotalScore);
+// var playerTwo = new Players(output2, player2CurrentScore, player2TotalScore);
+// // console.log(playerOne);
+
 // User Interface Logic
 $(document).ready(function() {
 
@@ -13,10 +29,23 @@ $(document).ready(function() {
     }); //end Start Game button
 
   $("#rollButton1").click(function() {
+    var player1CurrentScoreValue = 0;
 
-    var output = rollValue();
+    var output1 = rollValue();
+    player1CurrentScoreValue = player1CurrentScoreValue += output1;
+    console.log(player1CurrentScoreValue);
 
-    $("#player1CurrentScore").text(output);
+    $("#rollScore").text(player1CurrentScoreValue);
+    $("#player1CurrentScore").text(player1CurrentScoreValue);
+
+    $("#rollAgainButton1").click(function() {
+
+      var secondScore = rollValue();
+      console.log(secondScore);
+      var player1TotalScore = player1CurrentScoreValue += secondScore;
+      $("#rollScore").text(secondScore);
+      $("#player1CurrentScore").text(player1TotalScore);
+    })
 
   }); // roll Button close
 
