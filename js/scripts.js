065b81f9e0af382777335f698537ff2e5402798b
Nikki Wong and Talia Haller <talia@>
2018-08-23 16:39:00 -0700

    hide player1Screen when computer wins

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ Player.prototype.updateRollValue = function(newRollValue) {
 
 Player.prototype.hold = function() {
   var totalScore = this.totalScore += this.currentScore;
-  if (totalScore >= 25) {
+  if (totalScore >= 100) {
     return true;
   } else {
     return totalScore;
@@ -36,104 +36,104 @@ $(document).ready(function() {
   var playerTwo = new Player(2, 0, 0, 0);
   var numberOfTurns = [];
 
-//Start Game with Friend ---------------------- > -------------------
+  //Start Game with Friend ---------------------- > -------------------
 
-//Start Game
-    $("#playFriendButton").click(function() {
-      $("#startScreen").hide();
-      $("#player1Screen").show();
-      $("#scoreboard").show();
-      $("#homeScreen").show();
+  //Start Game
+      $("#playFriendButton").click(function() {
+        $("#startScreen").hide();
+        $("#player1Screen").show();
+        $("#scoreboard").show();
+        $("#homeScreen").show();
 
-  $("#rollButton1").click(function() {
-    var newRollValue = Roll();
-    var currentScore = playerOne.updateRollValue(newRollValue);
-    playerOne = new Player(1, newRollValue, currentScore, playerOne.totalScore);
-      $("#rollScore1").text(newRollValue);
-    $("#player1CurrentScore").text(playerOne.currentScore);
+    $("#rollButton1").click(function() {
+      var newRollValue = Roll();
+      var currentScore = playerOne.updateRollValue(newRollValue);
+      playerOne = new Player(1, newRollValue, currentScore, playerOne.totalScore);
+        $("#rollScore1").text(newRollValue);
+      $("#player1CurrentScore").text(playerOne.currentScore);
 
-    if (newRollValue === 1) {
+      if (newRollValue === 1) {
+        $("#switchPlayer1").show();
+        $("#rollButton1").hide();
+        $("#holdButton1").hide();
+        $(".player1TotalScore").text(playerOne.totalScore);
+      };
+    }); // roll Button close
+
+    $("#holdButton1").click(function() {
+      var totalScore = playerOne.hold();
+
+      if (totalScore === true) {
+        $("#winnerScreen").show();
+        $("#player1Screen").hide();
+        }
+
+      $(".player1TotalScore").text(playerOne.totalScore);
+      playerOne = new Player(1, 0, 0, totalScore);
+      $("#player1CurrentScore").text(playerOne.currentScore);
       $("#switchPlayer1").show();
       $("#rollButton1").hide();
       $("#holdButton1").hide();
-      $(".player1TotalScore").text(playerOne.totalScore);
-    };
-  }); // roll Button close
+    });
 
-  $("#holdButton1").click(function() {
-    var totalScore = playerOne.hold();
-
-    if (totalScore === true) {
-      $("#winnerScreen").show();
+    $("#switchPlayer1").click(function() {
       $("#player1Screen").hide();
-      }
+      $("#player2Screen").show();
+      $("#rollButton2").show();
+      $("#holdButton2").show();
+      $("#switchPlayer2").hide();
+    });
 
-    $(".player1TotalScore").text(playerOne.totalScore);
-    playerOne = new Player(1, 0, 0, totalScore);
-    $("#player1CurrentScore").text(playerOne.currentScore);
-    $("#switchPlayer1").show();
-    $("#rollButton1").hide();
-    $("#holdButton1").hide();
-  });
+    $("#rollButton2").click(function() {
+      var newRollValue2 = Roll();
+      var currentScore = playerTwo.updateRollValue(newRollValue2);
+      playerTwo = new Player(2, newRollValue2, currentScore, playerTwo.totalScore);
+        $("#rollScore2").text(newRollValue2);
+      $("#player2CurrentScore").text(playerTwo.currentScore);
 
-  $("#switchPlayer1").click(function() {
-    $("#player1Screen").hide();
-    $("#player2Screen").show();
-    $("#rollButton2").show();
-    $("#holdButton2").show();
-    $("#switchPlayer2").hide();
-  });
+      if (newRollValue2 === 1) {
+        $("#switchPlayer2").show();
+        $("#rollButton2").hide();
+        $("#holdButton2").hide();
+        $(".player2TotalScore").text(playerTwo.totalScore);
+      };
+    }); // roll Button close
 
-  $("#rollButton2").click(function() {
-    var newRollValue2 = Roll();
-    var currentScore = playerTwo.updateRollValue(newRollValue2);
-    playerTwo = new Player(2, newRollValue2, currentScore, playerTwo.totalScore);
-      $("#rollScore2").text(newRollValue2);
-    $("#player2CurrentScore").text(playerTwo.currentScore);
+    $("#holdButton2").click(function() {
+      var totalScore = playerTwo.hold();
 
-    if (newRollValue2 === 1) {
+      if (totalScore === true) {
+        $("#winnerScreen").show();
+        $("#player2Screen").hide();
+        }
+
+      $(".player2TotalScore").text(playerTwo.totalScore);
+      playerTwo = new Player(2, 0, 0, totalScore);
+      $("#player2CurrentScore").text(playerTwo.currentScore);
       $("#switchPlayer2").show();
       $("#rollButton2").hide();
       $("#holdButton2").hide();
-      $(".player2TotalScore").text(playerTwo.totalScore);
-    };
-  }); // roll Button close
+    });
 
-  $("#holdButton2").click(function() {
-    var totalScore = playerTwo.hold();
-
-    if (totalScore === true) {
-      $("#winnerScreen").show();
+    $("#switchPlayer2").click(function() {
       $("#player2Screen").hide();
-      }
+      $("#player1Screen").show();
+      $("#rollButton1").show();
+      $("#holdButton1").show();
+      $("#switchPlayer1").hide();
+    });
 
-    $(".player2TotalScore").text(playerTwo.totalScore);
-    playerTwo = new Player(2, 0, 0, totalScore);
-    $("#player2CurrentScore").text(playerTwo.currentScore);
-    $("#switchPlayer2").show();
-    $("#rollButton2").hide();
-    $("#holdButton2").hide();
-  });
-
-  $("#switchPlayer2").click(function() {
-    $("#player2Screen").hide();
-    $("#player1Screen").show();
-    $("#rollButton1").show();
-    $("#holdButton1").show();
-    $("#switchPlayer1").hide();
-  });
-
-  $("#playAgainButton").click(function() {
-    $("#player1Screen").show();
-    $("#winnerScreen").hide();
-    playerOne = new Player(1, 0, 0, 0);
-    playerTwo = new Player(2, 0, 0, 0);
-    $("#player1CurrentScore").text(playerOne.currentScore);
-    $(".player1TotalScore").text(playerOne.totalScore);
-    $("#player2CurrentScore").text(playerTwo.currentScore);
-    $(".player2TotalScore").text(playerTwo.totalScore);
-  });
-}); // End Game with Friend
+    $("#playAgainButton").click(function() {
+      $("#player1Screen").show();
+      $("#winnerScreen").hide();
+      playerOne = new Player(1, 0, 0, 0);
+      playerTwo = new Player(2, 0, 0, 0);
+      $("#player1CurrentScore").text(playerOne.currentScore);
+      $(".player1TotalScore").text(playerOne.totalScore);
+      $("#player2CurrentScore").text(playerTwo.currentScore);
+      $(".player2TotalScore").text(playerTwo.totalScore);
+    });
+  }); // End Game with Friend
 
 //Start Game with Computer ---------------------- > -------------------
     $("#playComputerButton").click(function() {
@@ -177,8 +177,8 @@ $(document).ready(function() {
   $("#switchPlayer1").click(function() {
     $("#player1Screen").hide();
     $("#player2Screen").show();
-    $("#rollButton2").show();
-    $("#holdButton2").show();
+    $("#rollButton2").hide();
+    $("#holdButton2").hide();
     $("#switchPlayer2").hide();
 
     setTimeout(function() {
@@ -196,9 +196,7 @@ $(document).ready(function() {
     $("#player2CurrentScore").text(playerTwo.currentScore);
 
     if (newRollValue2 === 1) {
-      $("#switchPlayer2").show();
-      $("#rollButton2").hide();
-      $("#holdButton2").hide();
+      $("#switchPlayer2").hide();
       $(".player2TotalScore").text(playerTwo.totalScore);
     };
 
@@ -218,17 +216,21 @@ $(document).ready(function() {
     if (totalScore === true) {
       $("#winnerScreen").show();
       $("#player2Screen").hide();
+      $("#player1Screen").hide();
       }
 
     $(".player2TotalScore").text(playerTwo.totalScore);
     playerTwo = new Player(2, 0, 0, totalScore);
     $("#player2CurrentScore").text(playerTwo.currentScore);
-    $("#switchPlayer2").show();
-    $("#rollButton2").hide();
-    $("#holdButton2").hide();
-    setTimeout(function() {
+    $("#switchPlayer2").hide();
+
+    console.log(totalScore);
+
+    if (totalScore != true) {
+      setTimeout(function() {
     $("#switchPlayer2").trigger("click");
-  }, 1500); // Computer SwitchPlayer2
+    }, 1500); // Computer SwitchPlayer2
+  };
   });
 
   $("#switchPlayer2").click(function() {
@@ -242,6 +244,9 @@ $(document).ready(function() {
 
   $("#playAgainButton").click(function() {
     $("#player1Screen").show();
+    $("#rollButton1").show();
+    $("#holdButton1").show();
+    $("#switchPlayer1").hide();
     $("#winnerScreen").hide();
     playerOne = new Player(1, 0, 0, 0);
     playerTwo = new Player(2, 0, 0, 0);
