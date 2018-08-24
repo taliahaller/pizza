cdb8ac6f2185f64fe05f6e8bed39160b1d944f8a
Student <example@example.com>
2018-08-24 10:33:46 -0700

    complete spec 4, cost is returned based on input after user hits input. No constructor or prototype in use currently.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,37 @@
 // Business Logic
 
+function Customer(name, numberOfToppings, pizzaSize) {
+  this.name = name;
+  this.numberOfToppings = numberOfToppings;
+  this.pizzaSize = pizzaSize;
+}
 
+function MyFunction (pizzaSizeCost) {
+  if (pizzaSizeCost === "small") {
+    return 7
+  }
+  if (pizzaSizeCost === "medium") {
+    return 10
+  }
+  if (pizzaSizeCost === "large") {
+    return 13
+  }
+  if (pizzaSizeCost === "extra large") {
+    return 15
+  }
+}
 
-
-
+function MyFunction2 (toppingCost) {
+  if (toppingCost === 1) {
+    return 1
+  }
+  if (toppingCost === 2) {
+    return 2
+  }
+  if (toppingCost === 3) {
+    return 3
+  }
+}
 
 // User Interface Logic
 $(document).ready(function() {
@@ -15,17 +43,24 @@ $(document).ready(function() {
     $("#orderInput").show();
   });
 
-
   // Submit Form
   $("form#pizzaOrderForm").submit(function() {
     event.preventDefault();
     $("#pizzaOrderCost").show();
 
-  // Put Name in Order Form
+  // Return Info in Return Section
   var customerNameInput = $("input#customer").val();
   var numberOfToppingsInput = $("input:checked").length;
   var pizzaSizeInput = $("option:selected").val();
-  $("#customerOutput").text(customerNameInput + ", your " + pizzaSizeInput + " pizza with " + numberOfToppingsInput + " topping(s) will cost " + "$20" + ".")
+
+  var costOfSize = MyFunction(pizzaSizeInput);
+  var costOfToppings = MyFunction2(numberOfToppingsInput);
+  console.log(costOfSize);
+  console.log(costOfToppings);
+  var costOfPizza = costOfSize += costOfToppings;
+  console.log(costOfPizza);
+
+  $("#customerOutput").text(customerNameInput + ", your " + pizzaSizeInput + " pizza with " + numberOfToppingsInput + " topping(s) will cost $" + costOfPizza + ".")
 
 
 
