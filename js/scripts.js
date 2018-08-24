327eae959bdaf8e0cd8eecddf3ca61952b031eec
Student <example@example.com>
2018-08-24 09:18:04 -0700

    add basic order form with basic CSS styling and JS button functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,10 @@
 // User Interface Logic
 $(document).ready(function() {
 
+  $("#getCostFormButton").click(function() {
+    $("#getCostFormButton").hide();
+    $(".well").show();
+  });
 
 
 }); // (document).ready close
