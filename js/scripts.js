b09419e7b4a1582857d31c470fedb7b5c1674891
Student <example@example.com>
2018-08-24 09:56:52 -0700

    complete first spec: Ask user for name and output that in Cost Answer Section.

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,25 @@
 // User Interface Logic
 $(document).ready(function() {
 
-
   // Open Cost Order Form
   $("#getCostFormButton").click(function() {
+    event.preventDefault();
     $("#getCostFormButton").hide();
     $("#orderInput").show();
   });
 
-  // Return Cost Section
-  $("#findCostButton").click(function() {
+
+  // Submit Form
+  $("form#pizzaOrderForm").submit(function() {
+    event.preventDefault();
     $("#pizzaOrderCost").show();
-  });
+
+  // Put Name in Order Form
+  var customerNameOutput = $("input#customer").val();
+  console.log(customerNameOutput);
+  $("#customerNameOutput").text(customerNameOutput);
 
 
 
+  }); // Form Close
 }); // (document).ready close
